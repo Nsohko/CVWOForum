0a967733b52239be9b9f91644de0f567319c2d4b
Sai <saibhagatsr@gmail.com>
2025-01-21 13:40:04 +0800

    Updated render.yaml

--- frontend/src/utils/apiClient.tsx
+++ frontend/src/utils/apiClient.tsx
@@ -2,7 +2,7 @@ import axios, { isAxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
     withCredentials: true, // Automatically include cookies
 });
 
