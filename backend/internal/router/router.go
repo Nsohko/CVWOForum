a95c18892ed339ecd46d75066912e23bdce80385
Sai <saibhagatsr@gmail.com>
2025-01-21 20:34:39 +0800

    Fix typo

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -15,11 +15,11 @@ func Setup() chi.Router {
 
 	// CORS middleware configuration
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "https://CVWObackend.onrender.com"}, // Frontend origins
-		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE"},                            // HTTP methods
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},                             // Headers allowed in requests
-		AllowCredentials: true,                                                                  // Allow cookies and credentials
-		MaxAge:           300,                                                                   // Cache preflight requests for 5 minutes
+		AllowedOrigins:   []string{"http://localhost:3000", "https://CVWOfrontend.onrender.com"}, // Frontend origins
+		AllowedMethods:   []string{"GET", "POST", "PATCH", "DELETE"},                             // HTTP methods
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},                              // Headers allowed in requests
+		AllowCredentials: true,                                                                   // Allow cookies and credentials
+		MaxAge:           300,                                                                    // Cache preflight requests for 5 minutes
 	}))
 
 	setUpRoutes(r)
